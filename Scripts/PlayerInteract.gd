f779165a535002b3e5d1785fdc10a1c0a2af85a1
JamesJDev <jellyjambusiness@gmail.com>
2025-03-09 18:58:47 -0400

    Basics of Player Interaction System

---
+++ Scripts/PlayerInteract.gd
@@ -0,0 +1,48 @@
+extends RayCast3D
+
+@export var selectedObject:Node3D
+@export var heldObject:Node3D
+@onready var rayCastEnd = $RayCastEnd
+@export var rayPos:Vector3
+
+
+func _ready():
+	pass
+
+func _unhandled_input(event):
+	# Check if the player interacts
+	if Input.is_action_just_pressed("interact") and selectedObject != null:
+		if selectedObject.has_method("interact") and is_colliding():
+			selectedObject.Interact()
+			return
+		# This is to drop held items
+		if heldObject:
+			selectedObject.Interact()
+			return
+
+
+func _process(_delta):
+	# Check if the raycast is colliding and that we aren't holding something
+	if heldObject == null:
+		if is_colliding():
+			# check if the object selected has changed
+			if selectedObject != get_collider():
+				ChangeSelected(get_collider())
+		else:
+			# If we are not colliding with any object
+			if selectedObject:
+				ChangeSelected(null)
+	# Move RayPos spot
+	if is_colliding():
+		rayPos = get_collision_point()
+	else:
+		rayPos = rayCastEnd.global_position
+	
+	
+		
+func ChangeSelected(new):
+	var old = selectedObject
+	selectedObject = new
+	print(selectedObject)
+	#emit_signal("selectedChange", old, new)
+	
