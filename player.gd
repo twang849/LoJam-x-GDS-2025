21dc89aae0e06bfc804cad51e33b751f17ba01e5
Tony Wang <twang849@uwo.ca>
2025-03-09 16:06:04 -0400

    Created script for player

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_backward"):
+		direction.z += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("jump"):
+		pass
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
