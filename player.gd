04753c83b7a8be83c7f7468b5105186d82836526
Tony Wang <twang849@uwo.ca>
2025-03-09 16:57:49 -0400

    Added camera

--- player.gd
+++ player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody3D
 
+@onready var _camera := $CameraPivot/SpringArm3D/Camera3D as Camera3D
+@onready var _camera_pivot := $CameraPivot as Node3D
+
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75) 
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
@@ -23,3 +29,21 @@ func _physics_process(delta):
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
+		$CameraPivot.basis = Basis.looking_at(direction)
+	
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if !is_on_floor():
+		target_velocity.y = target_velocity.y - (delta * fall_acceleration) 
+		
+	velocity = target_velocity
+	move_and_slide()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
+		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
+		_camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
+	
