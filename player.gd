01cd32580ad97fd188ba826360ddff22d572e528
James Johnson <jjohn538@uwo.ca>
2025-03-10 11:25:17 -0400

    Added Materials folder
    - Reduced Player Camera Sensitivity
    - Added Materials Folder
    - Changed Wall Prefab to have wood texture as a place holder
    - Changed Lighting in the demo scene

--- player.gd
+++ player.gd
@@ -2,8 +2,9 @@ extends CharacterBody3D
 
 @onready var _camera := $CameraPivot/Camera3D as Camera3D
 @onready var _camera_pivot := $CameraPivot as Node3D
+var direction = Vector3.ZERO
 
-@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.002
 @export var tilt_limit = deg_to_rad(75) 
 
 # How fast the player moves in meters per second.
@@ -18,18 +19,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
 	
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_backward"):
-		direction.z += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		target_velocity.y = jump_impulse
 		
 	
 	if direction != Vector3.ZERO:
@@ -50,6 +40,19 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func _unhandled_input(event: InputEvent) -> void:
+	direction = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_backward"):
+		direction.z += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
 	if event is InputEventMouseMotion:
 		_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
 		# Prevent the camera from rotating too far up or down. 
